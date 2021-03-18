fe3edc2b322ee80c3d5742d22944d400fe1e6e13
a448206058 <448206058@qq.com>
2021-03-18 17:09:19 +0800

    20210318

--- vue/dvue/src/component/main.js
+++ vue/dvue/src/component/main.js
@@ -1,31 +1,13 @@
 import dVue from "./index";
 
+dVue.component('my-button', {
+	template: '<button>hello</button>'
+})
+
 var vm = new dVue({
-	el: '#app',
-	data: {
-		items: [
-			'item1',
-			'item2',
-			'item3',
-		],
-		s1:''
-	},
-	template: '<ul><li class="1" :class="s1">11</li><li class="2">222</li><li class="3">333</li></ul>',
-	methods: {
-	},
 	components: {
-		item: {
-			props: ['msg'],
-			template: `<div>{{ msg }} <button @click="$emit('rm')">x</button></div>`
+		'my-button': {
+			template: '<button>world</button>'
 		}
 	},
 });
-
-dVue.component('button-counter', {
-	data: function(){
-		return {
-			count: 0
-		}
-	},
-	template: '<button @click="count++">{{count}}</button>'
-})
